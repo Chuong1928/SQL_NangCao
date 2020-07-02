b345b5151687d48a3de982f690d123e68db8fd54
NgoQuyTruong <37119899+Chuong1928@users.noreply.github.com>
2020-07-02 23:36:46 +0700

    Add files via upload

---
+++ baithuchanh3.sql
@@ -0,0 +1,382 @@
+﻿--ThiSatHachNangCao
+CREATE TABLE KHUVUC
+(
+	MaKV NVARCHAR(100) NOT NULL,
+	TenKhuVuc NVARCHAR(100) NOT NULL,
+	MoTa NVARCHAR(100) NOT NULL
+	CONSTRAINT PK_KHUVUC PRIMARY KEY CLUSTERED
+	(
+		MaKV ASC
+	) ON [PRIMARY]
+)
+ON [PRIMARY]
+GO
+
+
+CREATE TABLE LOAIXE
+(
+	MaLX NVARCHAR(100) NOT NULL,
+	MaKV NVARCHAR(100) NOT NULL,
+	TenLX NVARCHAR(100) NOT NULL,
+	MoTa NVARCHAR(100) NOT NULL
+	CONSTRAINT PK_LOAIXE PRIMARY KEY CLUSTERED
+	(
+		MaLX ASC
+	) ON [PRIMARY]
+)
+ON [PRIMARY]
+GO
+
+CREATE TABLE DOXE
+(
+	MaDX NVARCHAR(100) NOT NULL,
+	MaLX NVARCHAR(100) NOT NULL,
+	MaNV NVARCHAR(100) NOT NULL,
+	MaPhi NVARCHAR(100) NOT NULL,
+	BienSoXe NVARCHAR(100) NOT NULL,
+	TGVao DATETIME NOT NULL,
+	TGRa DATETIME NOT NULL
+	CONSTRAINT PK_DOXE PRIMARY KEY CLUSTERED
+	(
+		MaDX ASC
+	) ON [PRIMARY]
+)
+ON [PRIMARY]
+GO
+
+CREATE TABLE NHANVIEN
+(
+	MaNV NVARCHAR(100) NOT NULL,
+	TenNV NVARCHAR(100) NOT NULL,
+	NgaySinh DATE NOT NULL,
+	DiaChi NVARCHAR(100) NOT NULL,
+	SoDT NVARCHAR(100) NOT NULL
+	CONSTRAINT PK_NHANVIEN PRIMARY KEY CLUSTERED
+	(
+		MaNV ASC
+	) ON [PRIMARY]
+)
+ON [PRIMARY]
+GO
+
+CREATE TABLE PHIDOXE
+(
+	MaPhi NVARCHAR(100) NOT NULL,
+	TGbatdau TIME NOT NULL,
+	TGkethuc TIME NOT NULL,
+	MucPhi MONEY NOT NULL,
+	MoTa NVARCHAR(100)
+	CONSTRAINT PK_PHIDOXE PRIMARY KEY CLUSTERED
+	(
+		MaPhi ASC
+	) ON [PRIMARY]
+)
+ON [PRIMARY]
+GO
+
+
+ALTER TABLE dbo.DOXE WITH CHECK ADD CONSTRAINT FK_DOXE_LOAIXE
+FOREIGN KEY (MaLX) REFERENCES dbo.LOAIXE (MaLX)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE dbo.DOXE CHECK CONSTRAINT FK_DOXE_LOAIXE
+GO
+
+
+ALTER TABLE dbo.DOXE WITH CHECK ADD CONSTRAINT FK_DOXE_NHANVIEN
+FOREIGN KEY (MaNV) REFERENCES dbo.NHANVIEN (MaNV)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE dbo.DOXE CHECK CONSTRAINT FK_DOXE_NHANVIEN
+GO
+
+
+ALTER TABLE dbo.DOXE WITH CHECK ADD CONSTRAINT FK_DOXE_PHIDOXE
+FOREIGN KEY (MaPhi) REFERENCES dbo.PHIDOXE (MaPhi)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE dbo.DOXE CHECK CONSTRAINT FK_DOXE_PHIDOXE
+GO
+
+
+ALTER TABLE dbo.LOAIXE WITH CHECK ADD CONSTRAINT FK_LOAIXE_KHUVUC
+FOREIGN KEY (MaKV) REFERENCES dbo.KHUVUC (MaKV)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE dbo.LOAIXE CHECK CONSTRAINT FK_LOAIXE_KHUVUC
+GO
+
+
+SET NOCOUNT ON
+-------------------Khu vực-----------------------
+INSERT dbo.KHUVUC
+        ( MaKV, TenKhuVuc, MoTa )
+VALUES  ( N'KV02', -- MaKV - nvarchar(100)
+          N'Hai bánh', -- TenKhuVuc - nvarchar(100)
+          N'Dành cho các loại xe hai bánh'  -- MoTa - nvarchar(100)
+          )
+INSERT dbo.KHUVUC
+        ( MaKV, TenKhuVuc, MoTa )
+VALUES  ( N'KV02', -- MaKV - nvarchar(100)
+          N'Bốn bánh', -- TenKhuVuc - nvarchar(100)
+          N'Dành cho các loại xe bốn bánh'  -- MoTa - nvarchar(100)
+          )
+--------------------Loại xe-----------------------
+INSERT dbo.LOAIXE
+        ( MaLX, MaKV, TenLX, MoTa )
+VALUES  ( N'LX01', -- MaLX - nvarchar(100)
+          N'KV01', -- MaKV - nvarchar(100)
+          N'Xe hai bánh không có động cơ', -- TenLX - nvarchar(100)
+          N'Thuộc dòng xe đạp'  -- MoTa - nvarchar(100)
+          )
+INSERT dbo.LOAIXE
+        ( MaLX, MaKV, TenLX, MoTa )
+VALUES  ( N'LX02', -- MaLX - nvarchar(100)
+          N'KV01', -- MaKV - nvarchar(100)
+          N'Xe hai bánh có động cơ', -- TenLX - nvarchar(100)
+          N'Thuộc dòng xe máy'  -- MoTa - nvarchar(100)
+          )
+INSERT dbo.LOAIXE
+        ( MaLX, MaKV, TenLX, MoTa )
+VALUES  ( N'LX03', -- MaLX - nvarchar(100)
+          N'KV02', -- MaKV - nvarchar(100)
+          N'Xe bốn bánh', -- TenLX - nvarchar(100)
+          N'Thuộc dòng xe bốn bánh'  -- MoTa - nvarchar(100)
+          )
+--------------------Nhân viên-----------------------
+INSERT dbo.NHANVIEN
+        ( MaNV, TenNV, NgaySinh, DiaChi, SoDT )
+VALUES  ( N'NV001', -- MaNV - nvarchar(100)
+          N'Nguyễn Văn Nam', -- TenNV - nvarchar(100)
+          CAST('1988/11/10' AS DATE), -- NgaySinh - date
+          N'Đà Nẵng', -- DiaChi - nvarchar(100)
+          N'0905666222'  -- SoDT - nvarchar(100)
+          )
+INSERT dbo.NHANVIEN
+        ( MaNV, TenNV, NgaySinh, DiaChi, SoDT )
+VALUES  ( N'NV002', -- MaNV - nvarchar(100)
+          N'Hoàng Quyên', -- TenNV - nvarchar(100)
+          CAST('1995/02/21' AS DATE), -- NgaySinh - date
+          N'Đà Nẵng', -- DiaChi - nvarchar(100)
+          N'0905666223'  -- SoDT - nvarchar(100)
+          )
+INSERT dbo.NHANVIEN
+        ( MaNV, TenNV, NgaySinh, DiaChi, SoDT )
+VALUES  ( N'NV003', -- MaNV - nvarchar(100)
+          N'Nguyễn Anh Vân', -- TenNV - nvarchar(100)
+          CAST('1992/05/03' AS DATE), -- NgaySinh - date
+          N'Đà Nẵng', -- DiaChi - nvarchar(100)
+          N'0905666224'  -- SoDT - nvarchar(100)
+          )
+----------------------Phí đỗ xe-----------------------
+INSERT dbo.PHIDOXE
+        ( MaPhi ,
+          TGbatdau ,
+          TGkethuc ,
+          MucPhi ,
+          MoTa
+        )
+VALUES  ( N'P01' , -- MaPhi - nvarchar(100)
+          CAST('7:00' AS TIME), -- TGbatdau - time
+          CAST('23:00' AS TIME) , -- TGkethuc - time
+          1000, -- MucPhi - money
+          N'Xe gắn máy, xe đạp,ban ngày'  -- MoTa - nvarchar(100)
+        )
+INSERT dbo.PHIDOXE
+        ( MaPhi ,
+          TGbatdau ,
+          TGkethuc ,
+          MucPhi ,
+          MoTa
+        )
+VALUES  ( N'P02' , -- MaPhi - nvarchar(100)
+          CAST('7:00' AS TIME) , -- TGbatdau - time
+          CAST('23:00' AS TIME) , -- TGkethuc - time
+          5000 , -- MucPhi - money
+          N'Xe ô tô, ban ngày'  -- MoTa - nvarchar(100)
+        )
+INSERT dbo.PHIDOXE
+        ( MaPhi ,
+          TGbatdau ,
+          TGkethuc ,
+          MucPhi ,
+          MoTa
+        )
+VALUES  ( N'P03' , -- MaPhi - nvarchar(100)
+          CAST('23:00' AS TIME) , -- TGbatdau - time
+          CAST('7:00' AS TIME) , -- TGkethuc - time
+          3000 , -- MucPhi - money
+          N'Xe gắn máy, xe đạp, ban đêm'  -- MoTa - nvarchar(100)
+        )
+INSERT dbo.PHIDOXE
+        ( MaPhi ,
+          TGbatdau ,
+          TGkethuc ,
+          MucPhi ,
+          MoTa
+        )
+VALUES  ( N'P04' , -- MaPhi - nvarchar(100)
+		  CAST('23:00' AS TIME) , -- TGbatdau - time
+          CAST('7:00' AS TIME) , -- TGkethuc - time
+          10000 , -- MucPhi - money
+          N'Xe ô tô, ban đêm'  -- MoTa - nvarchar(100)
+        ) 
+----------------------Đỗ xe------------------------------------
+INSERT dbo.DOXE
+        ( MaDX ,
+          MaLX ,
+          MaNV ,
+          MaPhi ,
+          BienSoXe ,
+          TGVao ,
+          TGRa
+        )
+VALUES  ( N'DX001' , -- MaDX - nvarchar(100)
+          N'LX03' , -- MaLX - nvarchar(100)
+          N'NV001' , -- MaNV - nvarchar(100)
+          N'P01' , -- MaPhi - nvarchar(100)
+          N'43H-03941' , -- BienSoXe - nvarchar(100)
+          CAST('2015/11/07 7:00' AS DATETIME) , -- TGVao - datetime
+          CAST('2015/11/07 17:00' AS DATETIME)  -- TGRa - datetime
+        )
+INSERT dbo.DOXE
+        ( MaDX ,
+          MaLX ,
+          MaNV ,
+          MaPhi ,
+          BienSoXe ,
+          TGVao ,
+          TGRa
+        )
+VALUES  ( N'DX002' , -- MaDX - nvarchar(100)
+          N'LX01' , -- MaLX - nvarchar(100)
+          N'NV002' , -- MaNV - nvarchar(100)
+          N'P02' , -- MaPhi - nvarchar(100)
+          N'487' , -- BienSoXe - nvarchar(100)
+          CAST('2015/11/20 9:00' AS DATETIME) , -- TGVao - datetime
+          CAST('2015/11/20 10:00' AS DATETIME)  -- TGRa - datetime
+        )
+INSERT dbo.DOXE
+        ( MaDX ,
+          MaLX ,
+          MaNV ,
+          MaPhi ,
+          BienSoXe ,
+          TGVao ,
+          TGRa
+        )
+VALUES  ( N'DX003' , -- MaDX - nvarchar(100)
+          N'LX03' , -- MaLX - nvarchar(100)
+          N'NV001' , -- MaNV - nvarchar(100)
+          N'P04' , -- MaPhi - nvarchar(100)
+          N'75U-36584' , -- BienSoXe - nvarchar(100)
+          CAST('2015/12/21 9:00' AS DATETIME) , -- TGVao - datetime
+          CAST('2015/12/27 15:00' AS DATETIME)  -- TGRa - datetime
+        )
+
+SET NOCOUNT OFF
+
+/* CÂU 3b CSDL ThiSatHach*/
+/* 3a thực hiện đăng ký mới mỗi lượt đỗ xe thì hiển thị tổng số luowth đỗ xe của tất cả các loại xe cần đỗ*/
+
+CREATE TRIGGER cau1 ON dbo.DOXE
+AFTER INSERT
+AS
+	BEGIN
+		DECLARE @MALX NVARCHAR(100)
+		SELECT @MALX=MALX FROM Inserted
+		DECLARE @tong INT
+		SELECT @tong=COUNT(*)
+		FROM dbo.DOXE
+		WHERE MaLX=@MALX
+		BEGIN
+			PRINT N'Tổng số lượt đỗ xe cùng loại xe cần đỗ '+ CAST(@tong AS NVARCHAR(5))
+		END
+		
+	END
+GO
+/*test*/
+INSERT INTO dbo.DOXE
+        ( MaDX ,
+          MaLX ,
+          MaNV ,
+          MaPhi ,
+          BienSoXe ,
+          TGVao ,
+          TGRa
+        )
+VALUES  ( N'DX02' , -- MaDX - nvarchar(100)
+          N'LX03' , -- MaLX - nvarchar(100)
+          N'NV001' , -- MaNV - nvarchar(100)
+          N'P04' , -- MaPhi - nvarchar(100)
+          N'487' , -- BienSoXe - nvarchar(100)
+          '2014-11-30 07:00' , -- TGVao - datetime
+          '2014-12-15 07:00'  -- TGRa - datetime
+        )
+SELECT * FROM dbo.DOXE
+go
+/* câu 3b khi cập nhật thời gian vào và thời gian ra của một lượt đỗ xe , kiểm tra xem thời gian cập nhật đó
+có phù hợp hay không ( thời gian ra phải lơn hơn thời gian vào ít nhẩ 15 phút ) . nếu dữ liệu hợp lệ thì cho 
+phép cập nhật nếu không thì phải hiển thị thông báo" thời gian vào ohari lớn hơn thời gian ra ít nhất 15 "
+ALTER TRIGGER Trigger_2 ON dbo.DANGKYCUNGCAP
+AFTER UPDATE
+AS
+BEGIN
+	DECLARE @sodonghople INT
+	SELECT @sodonghople=COUNT(*) FROM Inserted 
+	WHERE Inserted.NgayBatDauCungCap>Inserted.NgayKetThucCungCap
+	IF(@sodonghople>0)
+		BEGIN
+			PRINT N'Ngày kết thúc cung cấp phải lớn hơn ngày bắt đầu cung cấp';
+			ROLLBACK TRANSACTION
+		END
+	 DECLARE @ngay INT
+	 SELECT @ngay= DATEDIFF(YEAR,Inserted.NgayBatDauCungCap,Inserted.NgayKetThucCungCap) FROM  Inserted
+	 IF(@ngay>4)
+		BEGIN
+			PRINT N'phải nhỏ hơn 4 năm'
+			ROLLBACK TRANSACTION
+		END
+END*/
+CREATE TRIGGER cau2 ON dbo.DOXE
+AFTER UPDATE
+AS
+	 BEGIN
+	     DECLARE @hople INT
+		 SELECT @hople=DATEDIFF(MINUTE,Inserted.TGVao,Inserted.TGRa) FROM Inserted
+		 IF(@hople>15)
+			BEGIN
+			    PRINT N'Lớn Hơn 15 Phút !! Không thể chèn!!'
+				ROLLBACK TRANSACTION
+			END
+	 END
+
+	UPDATE dbo.DOXE SET TGVao='2014-11-30 7:00',TGRa='2014-11-30 7:30'
+	WHERE MaDX='DX02'
+	GO
+    SELECT * FROM dbo.DOXE
+
+--câu 3  viết trigger_1 thực hiện đăng ký mới một lượt đỗ xe mới thì hiển thị tổng số lượt đỗ xe của tất cả các
+--xe cùng loại với xe cần đỗ
+GO
+SELECT * FROM dbo.DOXE INNER JOIN dbo.LoaiXe ON LoaiXe.MaLX = DOXE.MaLX
+GO
+
+CREATE TRIGGER cau3 ON dbo.DOXE
+AFTER INSERT
+AS
+	BEGIN
+		DECLARE @dem INT
+		DECLARE @malx CHAR(4)
+		SELECT @malx=  Inserted.MaLX FROM Inserted
+		SELECT @dem=COUNT(*) FROM dbo.DOXE WHERE MaLX=@malx
+		PRINT N'Tổng số lượt đỗ xe cỉa loại xe vừa nhập là '+CAST(@dem AS VARCHAR(5))
+	END
---
+++ baithuchanh4.sql
@@ -0,0 +1,545 @@
+﻿--tạo bảng
+CREATE TABLE HOCSINH
+( 
+	MaHocSinh NVARCHAR(50) PRIMARY KEY,
+	TenHocSinh NVARCHAR(50) NULL,
+	NamSinh NVARCHAR(50) NULL,
+	GioiTinh NVARCHAR(50) NULL,
+	TenLop NVARCHAR(50) NULL
+)
+ CREATE TABLE KHOI(
+	MaKhoi NVARCHAR(50) PRIMARY KEY,
+	TenKhoi NVARCHAR(50) NULL
+ )
+  CREATE TABLE DETHI(
+	MaDeThi NVARCHAR(50) PRIMARY KEY,
+	TenDeThi NVARCHAR(50) NULL,
+	ThoiGianThi NVARCHAR(50) NULL,
+	SoCauHoi NVARCHAR(50) NULL,
+	TrangThai NVARCHAR(50) NULL
+  )
+  CREATE TABLE CAUHOI (
+  MaCauHoi NVARCHAR(50) PRIMARY KEY,
+  MaKhoi NVARCHAR(50) NOT NULL,
+  NoiDung NVARCHAR(50) NULL,
+  DapAnA NVARCHAR(50) NULL,
+   DapAnB NVARCHAR(50) NULL,
+    DapAnC NVARCHAR(50) NULL,
+	 DapAnD NVARCHAR(50) NULL,
+	DapAnDung NVARCHAR(50) NULL
+  )
+CREATE TABLE DETHI_CAUHOI(
+	MaDeThi NVARCHAR(50) NOT NULL,
+	MaCauHoi NVARCHAR(50) NOT NULL
+	--CONSTRAINT DT_CH PRIMARY KEY (MaDeThi,MaCauHoi)
+)
+CREATE TABLE KETQUA(
+	MaHocSinh NVARCHAR(50) NOT NULL,
+	MaDeThi NVARCHAR(50) NOT NULL,
+	MaCauHoi NVARCHAR(50) NOT NULL,
+	DapAn NVARCHAR(50) NULL
+	--CONSTRAINT ketqua PRIMARY KEY (MaHocSinh,MaDeThi,MaCauHoi)
+)
+
+--tao diagram
+AlTER TABLE dbo.KETQUA
+ADD CONSTRAINT FK_KetQua_Hocsinh
+FOREIGN KEY (MaHocSinh)
+REFERENCES dbo.HOCSINH(MaHocSinh)
+ON UPDATE CASCADE
+ON DELETE NO ACTION
+
+AlTER TABLE dbo.KETQUA
+ADD CONSTRAINT FK_KetQua_Dethi
+FOREIGN KEY (MaDeThi)
+REFERENCES dbo.DETHI(MaDeThi)
+ON UPDATE CASCADE
+ON DELETE NO ACTION
+
+AlTER TABLE dbo.KETQUA
+ADD CONSTRAINT FK_KetQua_CauHoi
+FOREIGN KEY (MaCauHoi)
+REFERENCES dbo.CAUHOI(MaCauHoi)
+ON UPDATE CASCADE
+ON DELETE NO ACTION
+
+AlTER TABLE dbo.CAUHOI
+ADD CONSTRAINT FK_CH_KHOI
+FOREIGN KEY (MaKhoi)
+REFERENCES dbo.KHOI(MaKhoi)
+ON UPDATE CASCADE
+ON DELETE NO ACTION
+
+AlTER TABLE dbo.DETHI_CAUHOI
+ADD CONSTRAINT FK_DTCH_CH
+FOREIGN KEY (MaCauHoi)
+REFERENCES dbo.CAUHOI(MaCauHoi)
+ON UPDATE CASCADE
+ON DELETE NO ACTION
+
+AlTER TABLE dbo.DETHI_CAUHOI
+ADD CONSTRAINT FK_DTCH_DT
+FOREIGN KEY (MaDeThi)
+REFERENCES dbo.DETHI(MaDeThi)
+ON UPDATE CASCADE
+ON DELETE NO ACTION
+
+--insert data
+--hocsinh
+INSERT INTO dbo.HOCSINH
+        ( MaHocSinh ,
+          TenHocSinh ,
+          NamSinh ,
+          GioiTinh ,
+          TenLop
+        )
+VALUES  ( N'HS0001' , -- MaHocSinh - nvarchar(50)
+          N'HocSinhA' , -- TenHocSinh - nvarchar(50)
+          N'2013' , -- NamSinh - nvarchar(50)
+          N'Nam' , -- GioiTinh - nvarchar(50)
+          N'1/1'  -- TenLop - nvarchar(50)
+        )
+INSERT INTO dbo.HOCSINH
+        ( MaHocSinh ,
+          TenHocSinh ,
+          NamSinh ,
+          GioiTinh ,
+          TenLop
+        )
+VALUES  ( N'HS0002' , -- MaHocSinh - nvarchar(50)
+          N'HocSinhb' , -- TenHocSinh - nvarchar(50)
+          N'2012' , -- NamSinh - nvarchar(50)
+          N'Nu' , -- GioiTinh - nvarchar(50)
+          N'2/2'  -- TenLop - nvarchar(50)
+        )
+INSERT INTO dbo.HOCSINH
+        ( MaHocSinh ,
+          TenHocSinh ,
+          NamSinh ,
+          GioiTinh ,
+          TenLop
+        )
+VALUES  ( N'HS0003' , -- MaHocSinh - nvarchar(50)
+          N'HocSinhA' , -- TenHocSinh - nvarchar(50)
+          N'2012' , -- NamSinh - nvarchar(50)
+          N'Nu' , -- GioiTinh - nvarchar(50)
+          N'1/4'  -- TenLop - nvarchar(50)
+        )
+INSERT INTO dbo.HOCSINH
+        ( MaHocSinh ,
+          TenHocSinh ,
+          NamSinh ,
+          GioiTinh ,
+          TenLop
+        )
+VALUES  ( N'HS0004' , -- MaHocSinh - nvarchar(50)
+          N'HocSinhC' , -- TenHocSinh - nvarchar(50)
+          N'2010' , -- NamSinh - nvarchar(50)
+          N'Nam' , -- GioiTinh - nvarchar(50)
+          N'5/3'  -- TenLop - nvarchar(50)
+        )
+INSERT INTO dbo.HOCSINH
+        ( MaHocSinh ,
+          TenHocSinh ,
+          NamSinh ,
+          GioiTinh ,
+          TenLop
+        )
+VALUES  ( N'HS0005' , -- MaHocSinh - nvarchar(50)
+          N'HocSinhB' , -- TenHocSinh - nvarchar(50)
+          N'2013' , -- NamSinh - nvarchar(50)
+          N'Nu' , -- GioiTinh - nvarchar(50)
+          N'3/2'  -- TenLop - nvarchar(50)
+        )
+INSERT INTO dbo.HOCSINH
+        ( MaHocSinh ,
+          TenHocSinh ,
+          NamSinh ,
+          GioiTinh ,
+          TenLop
+        )
+VALUES  ( N'HS0006' , -- MaHocSinh - nvarchar(50)
+          N'HocSinhD' , -- TenHocSinh - nvarchar(50)
+          N'2010' , -- NamSinh - nvarchar(50)
+          N'Nu' , -- GioiTinh - nvarchar(50)
+          N'1/4'  -- TenLop - nvarchar(50)
+        )
+
+--khoi
+INSERT INTO dbo.KHOI
+        ( MaKhoi, TenKhoi )
+VALUES  ( N'K1', -- MaKhoi - nvarchar(50)
+          N'Khoi Lop 1'  -- TenKhoi - nvarchar(50)
+          )
+INSERT INTO dbo.KHOI
+        ( MaKhoi, TenKhoi )
+VALUES  ( N'K2', -- MaKhoi - nvarchar(50)
+          N'Khoi Lop 2'  -- TenKhoi - nvarchar(50)
+          )
+INSERT INTO dbo.KHOI
+        ( MaKhoi, TenKhoi )
+VALUES  ( N'K3', -- MaKhoi - nvarchar(50)
+          N'Khoi Lop 3'  -- TenKhoi - nvarchar(50)
+          )
+INSERT INTO dbo.KHOI
+        ( MaKhoi, TenKhoi )
+VALUES  ( N'K4', -- MaKhoi - nvarchar(50)
+          N'Khoi Lop 4'  -- TenKhoi - nvarchar(50)
+          )
+INSERT INTO dbo.KHOI
+        ( MaKhoi, TenKhoi )
+VALUES  ( N'K5', -- MaKhoi - nvarchar(50)
+          N'Khoi Lop 5'  -- TenKhoi - nvarchar(50)
+          )
+
+--dethi
+INSERT INTO dbo.DETHI
+        ( MaDeThi ,
+          TenDeThi ,
+          ThoiGianThi ,
+          SoCauHoi ,
+          TrangThai
+        )
+VALUES  ( N'DTA001' , -- MaDeThi - nvarchar(50)
+          N'De thi 1' , -- TenDeThi - nvarchar(50)
+          N'2017-11-20' , -- ThoiGianThi - nvarchar(50)
+          N'30' , -- SoCauHoi - nvarchar(50)
+          N'Da phat hanh'  -- TrangThai - nvarchar(50)
+        )
+INSERT INTO dbo.DETHI
+        ( MaDeThi ,
+          TenDeThi ,
+          ThoiGianThi ,
+          SoCauHoi ,
+          TrangThai
+        )
+VALUES  ( N'DTA002' , -- MaDeThi - nvarchar(50)
+          N'De thi 2' , -- TenDeThi - nvarchar(50)
+          N'2017-12-21' , -- ThoiGianThi - nvarchar(50)
+          N'20' , -- SoCauHoi - nvarchar(50)
+          N'Chua phat hanh'  -- TrangThai - nvarchar(50)
+        )
+INSERT INTO dbo.DETHI
+        ( MaDeThi ,
+          TenDeThi ,
+          ThoiGianThi ,
+          SoCauHoi ,
+          TrangThai
+        )
+VALUES  ( N'DTB001' , -- MaDeThi - nvarchar(50)
+          N'De thi 3' , -- TenDeThi - nvarchar(50)
+          N'2017-12-22' , -- ThoiGianThi - nvarchar(50)
+          N'30' , -- SoCauHoi - nvarchar(50)
+          N'Da phat hanh'  -- TrangThai - nvarchar(50)
+        )
+INSERT INTO dbo.DETHI
+        ( MaDeThi ,
+          TenDeThi ,
+          ThoiGianThi ,
+          SoCauHoi ,
+          TrangThai
+        )
+VALUES  ( N'DTB002' , -- MaDeThi - nvarchar(50)
+          N'De thi 4' , -- TenDeThi - nvarchar(50)
+          N'2016-09-10' , -- ThoiGianThi - nvarchar(50)
+          N'20' , -- SoCauHoi - nvarchar(50)
+          N'Da phat hanh'  -- TrangThai - nvarchar(50)
+        )
+INSERT INTO dbo.DETHI
+        ( MaDeThi ,
+          TenDeThi ,
+          ThoiGianThi ,
+          SoCauHoi ,
+          TrangThai
+        )
+VALUES  ( N'DTC001' , -- MaDeThi - nvarchar(50)
+          N'De thi 5' , -- TenDeThi - nvarchar(50)
+          N'2010-05-01' , -- ThoiGianThi - nvarchar(50)
+          N'25' , -- SoCauHoi - nvarchar(50)
+          N'Da phat hanh'  -- TrangThai - nvarchar(50)
+        )
+INSERT INTO dbo.DETHI
+        ( MaDeThi ,
+          TenDeThi ,
+          ThoiGianThi ,
+          SoCauHoi ,
+          TrangThai
+        )
+VALUES  ( N'DTD001' , -- MaDeThi - nvarchar(50)
+          N'De thi 7' , -- TenDeThi - nvarchar(50)
+          N'2017-12-21' , -- ThoiGianThi - nvarchar(50)
+          N'3' , -- SoCauHoi - nvarchar(50)
+          N'Da phat hanh'  -- TrangThai - nvarchar(50)
+        )
+--CAUHOI
+INSERT INTO dbo.CAUHOI
+        ( MaCauHoi ,
+          MaKhoi ,
+          NoiDung ,
+          DapAnA ,
+          DapAnB ,
+          DapAnC ,
+          DapAnD ,
+          DapAnDung
+        )
+VALUES  ( N'CH0001' , -- MaCauHoi - nvarchar(50)
+          N'K2' , -- MaKhoi - nvarchar(50)
+          N'ABC' , -- NoiDung - nvarchar(50)
+          N'DA A' , -- DapAnA - nvarchar(50)
+          N'DA B' , -- DapAnB - nvarchar(50)
+          N'DA C' , -- DapAnC - nvarchar(50)
+          N'DA D' , -- DapAnD - nvarchar(50)
+          N'A'  -- DapAnDung - nvarchar(50)
+        )
+INSERT INTO dbo.CAUHOI
+        ( MaCauHoi ,
+          MaKhoi ,
+          NoiDung ,
+          DapAnA ,
+          DapAnB ,
+          DapAnC ,
+          DapAnD ,
+          DapAnDung
+        )
+VALUES  ( N'CH0002' , -- MaCauHoi - nvarchar(50)
+          N'K2' , -- MaKhoi - nvarchar(50)
+          N'ABC' , -- NoiDung - nvarchar(50)
+          N'DA A' , -- DapAnA - nvarchar(50)
+          N'DA B' , -- DapAnB - nvarchar(50)
+          N'DA C' , -- DapAnC - nvarchar(50)
+          N'DA D' , -- DapAnD - nvarchar(50)
+          N'B'  -- DapAnDung - nvarchar(50)
+        )
+INSERT INTO dbo.CAUHOI
+        ( MaCauHoi ,
+          MaKhoi ,
+          NoiDung ,
+          DapAnA ,
+          DapAnB ,
+          DapAnC ,
+          DapAnD ,
+          DapAnDung
+        )
+VALUES  ( N'CH0003' , -- MaCauHoi - nvarchar(50)
+          N'K1' , -- MaKhoi - nvarchar(50)
+          N'ABC' , -- NoiDung - nvarchar(50)
+          N'DA A' , -- DapAnA - nvarchar(50)
+          N'DA B' , -- DapAnB - nvarchar(50)
+          N'DA C' , -- DapAnC - nvarchar(50)
+          N'DA D' , -- DapAnD - nvarchar(50)
+          N'A'  -- DapAnDung - nvarchar(50)
+        )
+INSERT INTO dbo.CAUHOI
+        ( MaCauHoi ,
+          MaKhoi ,
+          NoiDung ,
+          DapAnA ,
+          DapAnB ,
+          DapAnC ,
+          DapAnD ,
+          DapAnDung
+        )
+VALUES  ( N'CH0004' , -- MaCauHoi - nvarchar(50)
+          N'K3' , -- MaKhoi - nvarchar(50)
+          N'ABC' , -- NoiDung - nvarchar(50)
+          N'DA A' , -- DapAnA - nvarchar(50)
+          N'DA B' , -- DapAnB - nvarchar(50)
+          N'DA C' , -- DapAnC - nvarchar(50)
+          N'DA D' , -- DapAnD - nvarchar(50)
+          N'C'  -- DapAnDung - nvarchar(50)
+        )
+
+--dethi_cauhoi
+INSERT INTO dbo.DETHI_CAUHOI
+        ( MaDeThi, MaCauHoi )
+VALUES  ( N'DTA001', -- MaDeThi - nvarchar(50)
+          N'CH0001'  -- MaCauHoi - nvarchar(50)
+          )
+INSERT INTO dbo.DETHI_CAUHOI
+        ( MaDeThi, MaCauHoi )
+VALUES  ( N'DTA001', -- MaDeThi - nvarchar(50)
+          N'CH0002'  -- MaCauHoi - nvarchar(50)
+          )
+INSERT INTO dbo.DETHI_CAUHOI
+        ( MaDeThi, MaCauHoi )
+VALUES  ( N'DTA002', -- MaDeThi - nvarchar(50)
+          N'CH0003'  -- MaCauHoi - nvarchar(50)
+          )
+INSERT INTO dbo.DETHI_CAUHOI
+        ( MaDeThi, MaCauHoi )
+VALUES  ( N'DTB002', -- MaDeThi - nvarchar(50)
+          N'CH0004'  -- MaCauHoi - nvarchar(50)
+          )
+INSERT INTO dbo.DETHI_CAUHOI
+        ( MaDeThi, MaCauHoi )
+VALUES  ( N'DTD001', -- MaDeThi - nvarchar(50)
+          N'CH0001'  -- MaCauHoi - nvarchar(50)
+          )
+INSERT INTO dbo.DETHI_CAUHOI
+        ( MaDeThi, MaCauHoi )
+VALUES  ( N'DTD001', -- MaDeThi - nvarchar(50)
+          N'CH0002'  -- MaCauHoi - nvarchar(50)
+          )
+--KETQUA
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0001', -- MaHocSinh - nvarchar(50)
+          N'DTA001', -- MaDeThi - nvarchar(50)
+          N'CH0001', -- MaCauHoi - nvarchar(50)
+          N'A'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0001', -- MaHocSinh - nvarchar(50)
+          N'DTA001', -- MaDeThi - nvarchar(50)
+          N'CH0002', -- MaCauHoi - nvarchar(50)
+          N'B'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0001', -- MaHocSinh - nvarchar(50)
+          N'DTA002', -- MaDeThi - nvarchar(50)
+          N'CH0003', -- MaCauHoi - nvarchar(50)
+          N'C'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0002', -- MaHocSinh - nvarchar(50)
+          N'DTA001', -- MaDeThi - nvarchar(50)
+          N'CH0001', -- MaCauHoi - nvarchar(50)
+          N'D'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0002', -- MaHocSinh - nvarchar(50)
+          N'DTA001', -- MaDeThi - nvarchar(50)
+          N'CH0002', -- MaCauHoi - nvarchar(50)
+          N'C'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0002', -- MaHocSinh - nvarchar(50)
+          N'DTA002', -- MaDeThi - nvarchar(50)
+          N'CH0003', -- MaCauHoi - nvarchar(50)
+          N'B'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0003', -- MaHocSinh - nvarchar(50)
+          N'DTB002', -- MaDeThi - nvarchar(50)
+          N'CH0004', -- MaCauHoi - nvarchar(50)
+          N'A'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0004', -- MaHocSinh - nvarchar(50)
+          N'DTD001', -- MaDeThi - nvarchar(50)
+          N'CH0001', -- MaCauHoi - nvarchar(50)
+          N'A'  -- DapAn - nvarchar(50)
+          )
+INSERT INTO dbo.KETQUA
+        ( MaHocSinh, MaDeThi, MaCauHoi, DapAn )
+VALUES  ( N'HS0004', -- MaHocSinh - nvarchar(50)
+          N'DTD001', -- MaDeThi - nvarchar(50)
+          N'CH0002', -- MaCauHoi - nvarchar(50)
+          N'B'  -- DapAn - nvarchar(50)
+          )
+
+--câu 1a:
+go
+CREATE VIEW V_ThoiGianThi
+AS
+	SELECT HOCSINH.MaHocSinh,TenHocSinh,NamSinh,GioiTinh,TenDeThi FROM dbo.HOCSINH INNER  JOIN dbo.KETQUA ON KETQUA.MaHocSinh = HOCSINH.MaHocSinh 
+	INNER JOIN dbo.DETHI ON DETHI.MaDeThi = KETQUA.MaDeThi
+	WHERE GioiTinh=N'Nu' AND ThoiGianThi > '2017-11-20'
+GO
+SELECT * FROM dbo.V_ThoiGianThi
+--câu 1b
+UPDATE dbo.V_ThoiGianThi
+SET GioiTinh=NULL
+SELECT * FROM dbo.HOCSINH
+--CÂU 2a
+GO
+CREATE PROCEDURE Sp_XoaCauHoi @MaCauHoi NVARCHAR(50)
+AS
+	
+	BEGIN
+		DELETE FROM dbo.CAUHOI
+		WHERE MaCauHoi=@MaCauHoi
+	END
+
+DECLARE @check INT
+EXECUTE @check=dbo.Sp_XoaCauHoi @MaCauHoi = N'CHHAHA' -- nvarchar(50)
+--CÂU 2B
+GO
+CREATE PROCEDURE Sp_ThemNoiDungDT @MaDeThi NVARCHAR(50),@MaCauHoi NVARCHAR(50)
+AS
+	BEGIN
+		--kiểm tra @MaDeThi nhập vào đã tồn tại trong bẳng DETHI hay chưa
+		   DECLARE @check INT
+			SELECT @check=COUNT(*)
+			FROM dbo.DETHI
+			WHERE MaDeThi=@MaDeThi
+			IF(@check=0)
+			BEGIN
+				PRINT N'@MaDeThi Nhập vào chưa tồn tại trong bảng DETHI KHông thể chèn!!'
+			 	GOTO KETTHUC
+			END
+		--kiểm tra @MaCauHoi nhập vào đã tồn tại trong bảng CAUHOI hay chưa
+			SELECT @check=COUNT(*)
+			FROM dbo.CAUHOI
+			WHERE MaCauHoi=@MaCauHoi
+			IF(@check=0)
+			BEGIN
+				PRINT N'@MaCauHoi Nhập vào chưa tồn tại trong bảng CAUHOI KHông thể chèn!!'
+			 	GOTO KETTHUC
+			END
+			INSERT INTO dbo.DETHI_CAUHOI
+			        ( MaDeThi, MaCauHoi )
+			VALUES  ( @MaDeThi, -- MaDeThi - nvarchar(50)
+			          @MaCauHoi  -- MaCauHoi - nvarchar(50)
+			          )
+		 KETTHUC:
+	END
+	--test
+	DECLARE @test INT
+	EXECUTE @test=dbo.Sp_ThemNoiDungDT @MaDeThi = N'DTHAHA', -- nvarchar(50)
+	    @MaCauHoi = N'CHHAHA' -- nvarchar(50)
+-- câu 4
+GO
+CREATE FUNCTION func_Dem1()
+RETURNS INT
+AS
+	BEGIN
+	    DECLARE @dem INT
+		SELECT @dem=COUNT(MaDeThi) 
+		FROM dbo.DETHI
+		WHERE TrangThai=N'Chua phat hanh' AND YEAR(ThoiGianThi) BETWEEN 2016 AND 2017
+		RETURN @dem 
+	END
+GO
+SELECT dbo.func_Dem1()
+go
+--câu 5
+CREATE PROCEDURE Sp_XoaDeThi
+AS
+ BEGIN
+	CREATE TABLE BangTam(
+		MDThii NVARCHAR(50)
+	)
+	INSERT INTO dbo.BangTam
+	        (MDThii)
+	SELECT dbo.DETHI.MaDeThi FROM dbo.DETHI INNER JOIN dbo.DETHI_CAUHOI ON DETHI_CAUHOI.MaDeThi = DETHI.MaDeThi
+	WHERE (YEAR(ThoiGianThi) BETWEEN 2010 AND 2018) AND TrangThai=N'Da phat hanh'
+	
+	DELETE FROM dbo.DETHI
+	WHERE MaDeThi IN (SELECT MDThii 
+					FROM dbo.BangTam)
+	DELETE FROM dbo.DETHI_CAUHOI
+	WHERE MaDeThi IN (SELECT MDThii
+					FROM dbo.BangTam)
+
+ END
+ -- chưa chạy
+ DECLARE @n INT
+ EXECUTE @n=dbo.Sp_XoaDeThi
